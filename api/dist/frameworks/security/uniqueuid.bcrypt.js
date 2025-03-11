659e20746f2514da655c560048be81d504cc108b
Shanu Simon <shanusimon501@gmail.com>
2025-03-11 11:03:19 +0530

    Removed node_modules, .env, package-lock.json, and dist from tracking in both api and client

--- api/dist/frameworks/security/uniqueuid.bcrypt.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUniqueUid = void 0;
-const crypto_1 = require("crypto");
-const generateUniqueUid = (prefix = "auto_auction") => {
-    return `auto_auction-${prefix}-${(0, crypto_1.randomUUID)().slice(10)}`;
-};
-exports.generateUniqueUid = generateUniqueUid;
--- api/dist/shared/validation/phone.validation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.phoneNumberSchema = void 0;
-const zod_1 = require("zod");
-exports.phoneNumberSchema = zod_1.z
-    .string()
-    .length(10, { message: "Phone number must be exactly 10 digits" })
-    .regex(/^\d{10}$/, { message: "Phone number must contain only digits" });
