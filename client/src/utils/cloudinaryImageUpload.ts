53666fe97dd726023d14b8317a08d9bd3beba479
Shanu Simon <shanusimon501@gmail.com>
2025-07-05 19:55:43 +0530

    cloudinary URL changed to ENV

--- client/src/utils/cloudinaryImageUpload.ts
+++ client/src/utils/cloudinaryImageUpload.ts
@@ -3,6 +3,8 @@ import axios from "axios";
 export const uploadProfileImageCloudinary = async(file:File):Promise<string | null>=>{
     const CLOUDINARY_CLOUD_PRESET =import.meta.env.VITE_CLOUDINARY_PROFILE_UPLOAD_PRESET;
     const CLOUDINARY_CLOUD_NAME  = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+    const CLOUDINARY_CLOUD_URL = import.meta.env.VITE_CLOUDINARY_URL;
+    
 
     if(!CLOUDINARY_CLOUD_NAME || !CLOUDINARY_CLOUD_PRESET){
         console.error("Cloudinary API keys is missing");
@@ -15,7 +17,7 @@ export const uploadProfileImageCloudinary = async(file:File):Promise<string | nu
     formData.append("upload_preset",CLOUDINARY_CLOUD_PRESET);
 
     try {
-        const response = await axios.post(`https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,formData);
+        const response = await axios.post(`${CLOUDINARY_CLOUD_URL}${CLOUDINARY_CLOUD_NAME}/image/upload`,formData);
 
         if(response.status === 200){
             return response.data.secure_url;
